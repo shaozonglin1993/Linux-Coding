e800be962725937b2f023628d9c69eca8a933c80
shaozonglin1993 <shaozonglin321@163.com>
2024-03-27 17:36:23 +0800

    v

--- fork/fork.c
+++ fork/fork.c
@@ -1,8 +1,17 @@
+/*************************************************************************
+    > File Name: fork.c
+    > Author: szl
+    > Mail: shaozonglin321@163.com
+    > Created Time: 2024年03月27日 星期三 16时54分06秒
+ ************************************************************************/
+
 #include<stdio.h>
 #include<sys/types.h>
 #include<unistd.h>
+#include "fork.h"
 
-int main()
+
+int fork_test()
 {
 	int i = 0;
 	for(;i < 2;i++){
@@ -13,5 +22,6 @@ int main()
 			printf("father,pid:%d, id = %d\n",getpid(),id);
 		}
 	}
+
 	return 0;
 }
